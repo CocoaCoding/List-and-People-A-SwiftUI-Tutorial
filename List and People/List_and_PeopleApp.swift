9a006205cce84b155a71c6645bdc0277f5903ff5
Holger Hinzberg <holger@hinzberg.de>
2021-06-22 20:34:58 +0200

    Initial Commit

---
+++ List and People/List_and_PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_and_PeopleApp.swift
+//  List and People
+//
+//  Created by Holger Hinzberg on 22.06.21.
+//
+
+import SwiftUI
+
+@main
+struct List_and_PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
